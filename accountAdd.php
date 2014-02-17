b31436cc96560c967cb2c87ee6e6fa77adbf663e
Ivan Stojanov <ivan.stojnov1990@gmail.com>
2014-02-17 19:43:03 +0100

    change files

--- accountAdd.php
+++ accountAdd.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
-
+header('Access-Control-Allow-Methods: GET, POST');
 
 $return['accountName'] = /*json_decode*/($_POST['accountName']);
 //$return['accountType'] = json_decode($_POST['accountType']);
