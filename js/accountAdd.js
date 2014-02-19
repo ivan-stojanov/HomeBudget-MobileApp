8e4ede76f600e613fd63d19bc77ed4914a26b953
Ivan Stojanov <ivan.stojnov1990@gmail.com>
2014-02-19 20:58:02 +0100

    change files

--- js/accountAdd.js
+++ js/accountAdd.js
@@ -1,5 +1,38 @@
 function accoundAdd() {
 
-	alert($("#accountName").val() + " - " + $("#accountDate").val());
+	if(($('#accountName').val().length > 0) && ($('#accountType').val().length > 0) && ($('#accountBalance').val().length > 0) && ($('#accountDate').val().length > 0)){
+	
+		var html5rocks = {};
+		html5rocks.indexedDB = {};
+		var store;
+		html5rocks.indexedDB.db = null;
 
+		var openedDB = localStorage["openedDB"];	
+		var request = indexedDB.open(openedDB);
+
+		request.onsuccess = function(e) {
+		
+			html5rocks.indexedDB.db = e.target.result;			
+			var store = html5rocks.indexedDB.db.transaction(["accounts"], "readwrite").objectStore("accounts");	
+			$('#busy').hide();			
+			var obj =  { 
+						accountName: $("#accountName").val(),
+						accountType: $("#accountType").val(),
+						accountBalance: $("#accountBalance").val(),
+						accountDate: $("#accountDate").val()
+					};					
+			store.add(obj);
+			
+		};
+		
+		request.onerror = html5rocks.indexedDB.onerror;
+		//return true;
+		alert('New accound is added');
+		
+	} else {
+	
+        alert('Please fill all fields');
+		//return false;
+    }  
+	
 }
