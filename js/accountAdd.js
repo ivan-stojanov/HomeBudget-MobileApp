f761b6c3f10a85935222c82c74db6984e7862e7d
Ivan Stojanov <ivan.stojnov1990@gmail.com>
2014-02-20 10:49:18 +0100

    make changes

--- js/accountAdd.js
+++ js/accountAdd.js
@@ -1,19 +1,6 @@
-window.indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
-window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
-window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;	
-// Handle the prefix of Chrome to IDBTransaction/IDBKeyRange.
-if ('webkitIndexedDB' in window) {
-	window.IDBTransaction = window.webkitIDBTransaction;
-	window.IDBKeyRange = window.webkitIDBKeyRange;
-}
-// Hook up the errors to the console so we could see it. In the future, we need to push these messages to the user.
-indexedDB.onerror = function(e) {
-	console.log(e);
-	//alert('Error:' + e);
-};
 
-function accoundAdd() {
-
+function accountAdd() {
+	alert('accountAdd start');
 	if(($('#accountName').val().length > 0) && ($('#accountType').val().length > 0) && ($('#accountBalance').val().length > 0) && ($('#accountDate').val().length > 0)){
 	
 		var html5rocks = {};
@@ -25,7 +12,7 @@ function accoundAdd() {
 		var request = indexedDB.open(openedDB);
 
 		request.onsuccess = function(e) {
-		//	alert('request.onsuccess');
+			//alert('request.onsuccess');
 			html5rocks.indexedDB.db = e.target.result;			
 			var store = html5rocks.indexedDB.db.transaction(["accounts"], "readwrite").objectStore("accounts");	
 			$('#busy').hide();			
@@ -37,12 +24,15 @@ function accoundAdd() {
 					};					
 			store.add(obj);
 			alert('New accound is added');
-		//	alert('store.add');
+			alert('store.add');
 		};
 		
-		request.onerror = html5rocks.indexedDB.onerror;
-		//return true;
+		request.onerror = function(e) {
+			alert('Error:' + e);
+		}
 		
+		//return true;
+		alert('condition true');
 		
 	} else {
 	
