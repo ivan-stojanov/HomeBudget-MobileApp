6237602df8a21f75402e0c09812a2971cea9af13
Ivan Stojanov <ivan.stojnov1990@gmail.com>
2014-02-26 18:25:47 +0100

    modify income add

--- js/incomeAdd.js
+++ js/incomeAdd.js
@@ -1,3 +1,50 @@
+function addAccountsDropDown() {
+
+		var html5rocks = {};
+		html5rocks.indexedDB = {};
+		html5rocks.indexedDB.db = null;
+
+		openedDB = localStorage["openedDB"];	
+		request = indexedDB.open(openedDB);		
+
+		request.onsuccess = function(e) {
+			html5rocks.indexedDB.db = e.target.result;			
+			var store = html5rocks.indexedDB.db.transaction(["accounts"], "readwrite").objectStore("accounts");				
+			
+			var openedIndex = store.index("by_accountName");
+			var numItemsRequesr = openedIndex.count();	
+		//we need numItems because we need to find last item in the cursor and add the class "last child" so that is underlined
+			numItemsRequesr.onsuccess = function(evt) {   
+				var numItems = evt.target.result;	
+				/*alert(numItems);*/
+				if (openedIndex) {
+					var curCursor = openedIndex.openCursor(/*null, "prev"*/);				
+					curCursor.onsuccess = function(evt) {					
+						var cursor = evt.target.result;					
+						if (cursor) {
+							$('#drop-down-list-account').append('<option value="' + cursor.value.accountName + '">' + cursor.value.accountName + '</option>');
+							cursor.continue();
+						}
+					}
+				}	
+			}			
+			//$('#drop-down-list-account').append('<option value="test1">Test1: 1 day</option>');
+			/*alert('request.onsuccess!');*/
+		};
+		
+		request.onupgradeneeded = function(e) {  
+			alert('request.onupgradeneeded!');
+		}
+		
+		request.onerror = function(e) {
+			alert('request.onerror!');
+		}
+}
+
+function addCategoriesDropDown() {	
+	$('#drop-down-list-category').append('<option value="testCategory">testCategory</option>');
+}
+
 function funcAccountAdd() {
 	var openedDB;
 	var request;
