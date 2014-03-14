699df4cbe8314d0240ef968b94ae6b8103f91340
Ivan Stojanov <ivan.stojnov1990@gmail.com>
2014-03-14 11:59:01 +0100

    detele expense different ways (bill or not)

--- js/expenseDetails.js
+++ js/expenseDetails.js
@@ -52,7 +52,10 @@ html5rocks.indexedDB.open = function() {
 		requestID.onsuccess = function(e) {	
 			var result = event.target.result;
 			if(!!result == false){alert(result);}
-			
+
+			if(result.expenseCategory == "Bill") {
+				document.getElementById('spanMSG').innerHTML="Mark this Bill as UnPaid";
+			}	
 			$('#expenseName').text(result.expenseName);
 			$('#expenseAmmount').text(result.expenseAmmount);
 			$('#expenseAccount').text(result.expenseAccount);
@@ -76,29 +79,72 @@ $( document ).ready(function() {
 	$(".confirmDelete").on("click", function(event){
 	
 	//alert(getExpenseID);
+	//PREKU NEKOJ INDEKS IZMINI GI SITE I KOGA KE DOJDES DO getExpenseID ZACUVAJ SE ZA NEGO
+	//AKO NE E BILL IZBRISI GO SO KODOT VO PRODOLZENIE
+	//AKO E BILL ZAMENI GO STATUSO VO UNPAID, SO KODOT OD billDetails.js - LINE 120
 	
-	
-		if(confirm("Are you sure you want to delete this expense?")){		
-			var html5rocks = {};
-			html5rocks.indexedDB = {};
-			html5rocks.indexedDB.db = null;
-			var openedDB = localStorage["openedDB"];	
-			var requestDelete = indexedDB.open(openedDB);	
-				requestDelete.onsuccess = function(e) {  
-				
-					html5rocks.indexedDB.db = e.target.result;
-					var storeDelete = html5rocks.indexedDB.db.transaction(["expenses"], "readwrite").objectStore("expenses");	
-					storeDelete.delete(parseInt(getExpenseID));
-					alert("This expense is deleted!");
-					return true;
+		var html5rocks = {};
+		html5rocks.indexedDB = {};
+		html5rocks.indexedDB.db = null;
+		var openedDB = localStorage["openedDB"];	
+		var requestDelete = indexedDB.open(openedDB);	
+		
+		requestDelete.onsuccess = function(e) {  
+		
+			html5rocks.indexedDB.db = e.target.result;
+			var storeDelete = html5rocks.indexedDB.db.transaction(["expenses"], "readwrite").objectStore("expenses");
+			
+			var replace = new Object;	
+			var openedIndexFindThisID = storeDelete.index("by_expenseName");
+			var cursorFindThisID = openedIndexFindThisID.openCursor();	
+			cursorFindThisID.onsuccess = function (ev){
+				var cursorThisID = ev.target.result;
+				if (cursorThisID){
+					if(cursorThisID.value["id"] == (parseInt(getExpenseID))){
+						replace.id = cursorThisID.value.id;
+						replace.expenseAccount = cursorThisID.value.expenseAccount;
+						replace.expenseAmmount = cursorThisID.value.expenseAmmount;
+						if( cursorThisID.value.expenseBillPaid == "paidNo" ){ replace.expenseBillPaid = "paidYes"; }
+						else			 									{ replace.expenseBillPaid = "paidNo";  }
+						replace.expenseCategory = cursorThisID.value.expenseCategory;
+						replace.expenseDueDate = cursorThisID.value.expenseDueDate;
+						replace.expenseName = cursorThisID.value.expenseName;
+						if(cursorThisID.value.expenseRepeatCycle == '') 	{ replace.expenseRepeatCycle = ''; } 
+						else 			 { replace.expenseRepeatCycle = cursorThisID.value.expenseRepeatCycle; }
+						if(cursorThisID.value.expenseRepeatEndDate == '')	{ replace.expenseRepeatEndDate = ''; }
+						else			 { replace.expenseRepeatEndDate = cursorThisID.value.expenseRepeatEndDate; }
+					}
+					cursorThisID.continue();
+				} else {
+					if(replace.expenseCategory != 'Bill') {	
+						if(confirm("Are you sure you want to delete this expense?")){		
+							storeDelete.delete(parseInt(getExpenseID));
+							alert("This expense is deleted!");
+							window.location.href = "expensesList.html";
+							return true;
+						} else {
+							event.preventDefault();
+							return false;
+						}							
+					} else {
+						if(confirm("Are you sure you want to mark this bill as UnPaid?")){		
+							storeDelete.delete(parseInt(getExpenseID));
+							storeDelete.add(replace);
+							alert("This bill is marked as UnPaid!");
+							window.location.href = "expensesList.html";						
+							return true;
+						} else {
+							event.preventDefault();
+							return false;
+						}		
+					}	
 				}
-				
-				requestDelete.onerror = function(e) {
-					alert('request.onerror!');
-				}				
-		} else {
-			event.preventDefault();
-			return false;
-		}		
+			}
+		}			
+		
+		requestDelete.onerror = function(e) {
+			alert('request.onerror!');
+		}				
+		
 	});
 });
